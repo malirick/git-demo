4787c948db79f0f0c07bc17b78ba33bb54fe2c9c
rick <747404919@qq.com>
2022-09-10 21:26:01 +0800

    9/10 快速开发完成

--- gulimall-product/src/test/java/com/rick/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/rick/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,29 @@
 package com.rick.gulimall.product;
 
+import com.rick.gulimall.product.dao.BrandDao;
+import com.rick.gulimall.product.entity.BrandEntity;
+import com.rick.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@EnableAutoConfiguration
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired()
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+         brandEntity.setName("oppo");
+
+        brandService.save(brandEntity);
+        System.out.println("插入成功");
+
+
     }
 
 }
