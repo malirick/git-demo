e91c7d1b2ceae6f6efa2f48d6894eaf48a03522a
malirick <102168277+malirick@users.noreply.github.com>
2022-11-10 15:50:03 +0800

    Update OssController.java

--- gulimall-third-party/src/main/java/com/rick/gulimall/thirdparty/controller/OssController.java
+++ gulimall-third-party/src/main/java/com/rick/gulimall/thirdparty/controller/OssController.java
@@ -30,10 +30,10 @@ public class OssController {
         //https://gulimall-clouds.oss-cn-beijing.aliyuncs.com/iqiyi.png
 
         // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
+        String endpoint = "oss-cn-h";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5t";
-        String accessKeySecret = "FfbZsNE";
+        String accessKeyId = "LT";
+        String accessKeySecret = "F";
 
         String bucket = "gulimall-rick"; // 请填写您的 bucketname 。
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
